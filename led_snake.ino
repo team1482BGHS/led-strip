94ffdee8c7581f66787d972dcbeff46f317a8901
Team 1482 <frc1482@gmail.com>
2019-01-30 18:11:23 -0700

    Update led_snake.ino

--- led_snake.ino
+++ led_snake.ino
@@ -1,78 +1,56 @@
 #include "FastLED.h"
 
-#define NUM_LEDS 150
+#define NUM_LEDS 88
+#define SWITCH_PIN 7
 #define DATA_PIN 8
-#define BUTTON_PIN 2
 
-#define SNAKE_LENGTH 5
-#define SNAKE_SPEED 5
+#define SNAKE_LENGTH 20
+#define SNAKE_SPEED 50
 
 CRGB leds[NUM_LEDS];
+int red_color[3] = {255, 0, 0};
+int blue_color[3] = {0, 0, 255};
 
 void setup() {
   Serial.begin(9600);
   
   delay(1000);
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  pinMode(BUTTON_PIN, INPUT);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  pinMode(SWITCH_PIN, INPUT);
 
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::Black;
   }
 }
 
-int teamColor = 0; // 0: red, 1: blue, 2: green
-int index = 0;
-bool reverse = false;
-bool lastState = false;
-int highFor = 0;
-
 void loop() {
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-    highFor++;
+  bool switch_state = digitalRead(SWITCH_PIN) == true;
 
-    if (highFor >= 1000 / SNAKE_SPEED) {
-      highFor = 0;
-      Serial.write("on\n");
-      teamColor++;
-      if (teamColor > 2) {
-        teamColor = 0;
-      }
+  for (int i = 0; i < NUM_LEDS - SNAKE_LENGTH + 1; i++) {
+    for (int j = 0; j < SNAKE_LENGTH; j++) {
+      leds[i + j].setRGB(switch_state ? red_color[0] : blue_color[0], switch_state ? red_color[1] : blue_color[1], switch_state ? red_color[2] : blue_color[2]);
     }
-  } else {
-    highFor = 0;
-  }
-  // Serial.write(buttonState ? "1\n" : "0\n");
 
-  
-  char number[2];
-  itoa(index, number, 10);
-  // Serial.write(number);
-  // Serial.write("\n");
-  
-    
-  for (int i = -1; i < SNAKE_LENGTH + 1; i++) {
-    if (index + i < NUM_LEDS) {
-      leds[index + i].setRGB(teamColor == 2 ? 255 : 0, teamColor == 0 ? 255 : 0, teamColor == 1 ? 255 : 0);
-      if (i == -1 || i == SNAKE_LENGTH) {
-        leds[index + i].setRGB(0, 0, 0);
-      }
+    FastLED.show();
+    delay(1000 / SNAKE_SPEED);
+
+    for (int j = 0; j < SNAKE_LENGTH; j++) {
+      leds[i + j] = CRGB::Black;
     }
-    
   }
 
-  FastLED.show();
-  delay(SNAKE_SPEED);
+  switch_state = digitalRead(SWITCH_PIN) == true;
 
-  if (!reverse) {
-    index++;
-  } else {
-    index--;
-  }
-  
-  if (index >= NUM_LEDS) {
-    reverse = true;
-  } else if (index < 0) {
-    reverse = false;
+  for (int i = NUM_LEDS - 1; i > SNAKE_LENGTH; i--) {
+    for (int j = 0; j < SNAKE_LENGTH; j++) {
+      leds[i - j].setRGB(switch_state ? red_color[0] : blue_color[0], switch_state ? red_color[1] : blue_color[1], switch_state ? red_color[2] : blue_color[2]);
+    }
+
+    FastLED.show();
+    delay(1000 / SNAKE_SPEED);
+
+    for (int j = 0; j < SNAKE_LENGTH; j++) {
+      leds[i - j] = CRGB::Black;
+    }
   }
 }
